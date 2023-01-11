8a39750c639d2af743fad5a25b162102de7e6815
Fabian Kongelf <fkongelf@gmail.com>
2023-01-11 14:40:03 +0100

    changed config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Vortex docs',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://vortexntnu.github.io',
+  baseUrl: '/vortex-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
