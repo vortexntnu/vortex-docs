cdc89fbcf65adaf138f88b99214a8aac62a2f6e6
fabiako <fabiako@stud.ntnu.no>
2023-01-15 16:50:17 +0100

    now?

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,37 +1,35 @@
 import React from 'react';
-import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import EditorForm from '@site/src/components/EditorForm';
 
 import styles from './index.module.css';
 import Stackedit from 'stackedit-js';
 
-let save;
-const stackedit = new Stackedit();
-
-function openStackEdit() {
-  // Open the iframe
-  stackedit.openFile({
-    name: 'Filename', // with an optional filename
-    content: {
-      text: `<!--\n\tFirst bracket is frontmatter and contain metadata about the file.\n\tThe page's content start at "Page title".\n-->\n\n---\n\ttitle: page name\n\tdescription: page description\n---\n\n# Page title\n`,
-    }
-  });
-}
-
-// Listen to StackEdit events and apply the changes to the save variable, basiclly auto save.
-stackedit.on('fileChange', (file) => {
-  save = file.content.text;
-});
-
-// When the editor is closed
-stackedit.on('close', () => {
-  console.log(save);
-});
 
 export default function Home() {
-  // const {siteConfig} = useDocusaurusContext();
+  let save;
+  const stackedit = new Stackedit();
+
+  function openStackEdit() {
+    // Open the iframe
+    stackedit.openFile({
+      name: 'Filename', // with an optional filename
+      content: {
+        text: `<!--\n\tFirst bracket is frontmatter and contain metadata about the file.\n\tThe page's content start at "Page title".\n-->\n\n---\n\ttitle: page name\n\tdescription: page description\n---\n\n# Page title\n`,
+      }
+    });
+  }
+
+  // Listen to StackEdit events and apply the changes to the save variable, basiclly auto save.
+  stackedit.on('fileChange', (file) => {
+    save = file.content.text;
+  });
+
+  // When the editor is closed
+  stackedit.on('close', () => {
+    console.log(save);
+  });
+
   return (
     <Layout
       title={`Editor`}
